66365962b1e934c57ec7567f61439fb9d9d277a1
seb <seb.hallx@gmail.com>
2020-07-16 23:02:05 +0100

    added powerbar

--- Projectiles.py
+++ Projectiles.py
@@ -10,6 +10,21 @@ arrow_img = pygame.image.load("./assets/arrow/90arrow.png")
 def round_to_angle(x, angle):
     return int(round(x / angle)) * angle
 
+class Powerbar:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.power = 150
+        self.change = 3
+
+    def draw(self):
+        rect = pygame.Rect(self.x, self.y, self.power, 100)
+        pygame.draw.rect(screen, (255, 255, 255), rect)
+
+    def update(self):
+        if self.power == 300 or self.power == 3:
+            self.change *= -1
+        self.power += self.change
 
 class Arrow:
     def __init__(self, x, y, u, angle):
@@ -42,22 +57,31 @@ class Arrow:
         self.y -= self.time_inc * vertical_v - 0.5 * 9.81 * self.time_inc ** 2
 
 
-screen_width, screen_height = 1600, 800
+screen_width, screen_height = 3000, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
-arrow = Arrow(10, 500, 100, math.pi/2.5)
+arrow = Arrow(0, 800, 100, math.pi/4)
+powerbar = Powerbar(100, 100)
 
+arrow_group = []
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                new_arrow = Arrow(0, 800, powerbar.power, math.pi/4)
+                arrow_group.append(new_arrow)
 
     screen.fill((0, 0, 0))
-    arrow.draw()
-    arrow.update()
+    powerbar.draw()
+    powerbar.update()
+    for arrow in arrow_group:
+        arrow.draw()
+        arrow.update()
 
     pygame.display.flip()
     clock.tick(60)
