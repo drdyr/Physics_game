3cd200af20f09f0a71b7ede3250d491849704119
seb <seb.hallx@gmail.com>
2020-07-16 19:15:38 +0100

    image now changes adjusting to the real flight angle

--- Projectiles.py
+++ Projectiles.py
@@ -4,7 +4,11 @@ import math
 
 pygame.init()
 
-arrow_img = pygame.image.load("./assets/arrow.png")
+arrow_img = pygame.image.load("./assets/90arrow.png")
+
+
+def round_to_angle(x, angle):
+    return int(round(x / angle)) * angle
 
 
 class Arrow:
@@ -14,29 +18,27 @@ class Arrow:
         self.x = x
         self.y = y
         self.t = 0
-        self.image = pygame.image.load("./assets/arrow.png")
-        self.rect = self.image.get_rect(center=(self.x, self.y))
         self.horizontal_u = math.cos(self.angle) * self.u
         self.vertical_u = math.sin(self.angle) * self.u
-
 
     def flight_angle(self):
         vertical_v = self.vertical_u - 9.81 * self.t
         flight_angle = math.atan(vertical_v / self.horizontal_u)
         flight_angle *= 180 / math.pi
-        return round(flight_angle, 2)
+        return int(flight_angle)
 
     def draw(self):
-        self.rot_center(10)
-        rect = self.image.get_rect(center=(self.x, self.y))
-        screen.blit(self.image, rect)
+        flight_angle = self.flight_angle()
+        pic_angle = round_to_angle(flight_angle, 15)
+        image = pygame.image.load("./assets/" + str(pic_angle) + "arrow.png")
+        rect = image.get_rect(center=(self.x, self.y))
+        screen.blit(image, rect)
 
     def update(self):
         vertical_v = self.vertical_u - 9.81 * self.t
-        print(self.flight_angle())
-        self.t += 1/6
-        self.x += 1/6 * self.horizontal_u
-        self.y -= 1/6 * vertical_v - 0.5 * 9.81 * (1/6) ** 2
+        self.t += 1
+        self.x += 1 * self.horizontal_u
+        self.y -= 1 * vertical_v - 0.5 * 9.81 * (1) ** 2
 
     def rot_center(self, angle):
         center = self.rect.center
@@ -48,7 +50,7 @@ screen_width, screen_height = 1600, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
-arrow = Arrow(10, 500, 50, math.pi/2.5)
+arrow = Arrow(10, 500, 100, math.pi/2.5)
 
 
 while True:
@@ -65,4 +67,4 @@ while True:
     arrow.update()
 
     pygame.display.flip()
-    clock.tick(6)
+    clock.tick(30)
